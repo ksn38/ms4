2de6fbbd7113c27596846644dbef217b00b9890f
ksn38 <ksn38@yandex.ru>
2024-04-24 21:21:51 +0300

    add scraperTicker

--- laravel/scraperTicker.php
+++ laravel/scraperTicker.php
@@ -1,8 +1,23 @@
 <?php
-require 'vendor/autoload.php';
+use Illuminate\Contracts\Console\Kernel;
+
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+$kernel = $app->make(Kernel::class);
+$kernel->bootstrap();
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Support\Facades\Config;
+
+$capsule = new Capsule;
+$capsule->addConnection(Config::get('database'));
+$capsule->setAsGlobal();  //this is important
+$capsule->bootEloquent();
 
 function ticks(...$args)
 {
+    $tickers = [];
     $setTicks = new \Ds\Set(['wti', 'gold', 'sz', 'wheat', 'ss', 'cop']);
     $commodities = ['wti'=> 'CL=F', 'gold'=> 'GC=F', 'wheat'=> 'KE=F', 'sz'=> '399001.SZ', 'ss'=> '000001.SS', 'cop'=> 'HG=F'];
 
@@ -22,12 +37,20 @@ function ticks(...$args)
                 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
             ]
         ]);
-        print($response->getBody());
-        /*parsed_html = bs(response, 'lxml')
-        t = parsed_html.find('fin-streamer', {'class': 'livePrice svelte-mgkamr'}).text.replace(',', '')
-        #print(t)
-        t_dict[i] = float(t)*/
+        $out = [];
+        preg_match("/livePrice svelte-mgkamr.*?<span>/", $response->getBody(), $out);
+        $out2 = [];
+        preg_match("/[\d\.]{2,}/", $out[0], $out2);
+        $tickers[$i] = floatval($out2[0]);
         }
+    return $tickers;
 }
 
-ticks('sz');
+$dateToday = date("Y-m-d");
+//$date7 = (date.today() - timedelta(7)).strftime("%Y-%m-%d");
+$tickers = DB::table('tickers')->where('date_added', '=', $dateToday)->get();
+
+//ticks('sz');
+$x = DB::select("select gspc from tickers order by date_added desc limit 1");
+//print_r(count($tickers));
+//var_dump(floatval($x[0]->gspc) == ticks('gspc')['gspc']);
